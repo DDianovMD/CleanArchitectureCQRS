31f08c50e3c43a7796ba88e211a4ec170643d6dc
DDianovMD <ddianovmd@abv.bg>
2026-09-09 01:58:42 +0300

    Added logic for loading employees and displaying them.

--- src/front-end-react/src/pages/employees/employees.tsx
+++ src/front-end-react/src/pages/employees/employees.tsx
@@ -1,7 +1,52 @@
-import type { JSX } from "react";
+import "./employees.css";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { useEffect, useState, type JSX } from "react";
+import axiosInstance from "../../services/ApiService";
+import type { Employee } from "../../interfaces/Employee";
+import { Skeleton } from "primereact/skeleton";
 
 export default function EmployeesPage(): JSX.Element {
-    return <>
-        <div>Employees page works!</div>
-    </>
+  const [employees, setEmployees] = useState<Array<Employee>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  async function fetchEmployees() {
+    setLoading(true);
+    const response = await axiosInstance.get<Array<Employee>>('/employee');
+    setEmployees(response.data);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500); // Simulate a delay for loading state
+  };
+
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      try {
+        await fetchEmployees();
+      } catch (error) {
+        console.error('Error fetching employees:', error);
+      }
+    };
+    fetchInitialData();
+  }, []);
+
+  return <div className="employees-page-container">
+    {
+      loading && <DataTable value={employees} stripedRows>
+        <Column field="id" header="Id" style={{ width: '25%' }} body={<Skeleton />}></Column>
+        <Column field="firstName" header="First name" style={{ width: '25%' }} body={<Skeleton />}></Column>
+        <Column field="lastName" header="Last name" style={{ width: '25%' }} body={<Skeleton />}></Column>
+        <Column field="address" header="Address" style={{ width: '25%' }} body={<Skeleton />}></Column>
+      </DataTable>
+    }
+    {
+      !loading && <DataTable value={employees} stripedRows tableStyle={{ minWidth: '50rem' }}>
+        <Column field="id" header="Id"></Column>
+        <Column field="firstName" header="First name"></Column>
+        <Column field="lastName" header="Last name"></Column>
+        <Column field="address" header="Address"></Column>
+      </DataTable>
+    }
+  </div>
 }
